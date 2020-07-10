7be1f7fc1664a91eefc9a7529f449c5cac47b155
Carl Redding <credding07@gmail.com>
2020-07-10 09:37:22 -0400

    added the list comprehension function to the file.

--- Whiteboard/whiteboard.py
+++ Whiteboard/whiteboard.py
@@ -25,6 +25,7 @@ looking like i have a nested for loop
 '''
 
 arr = [[8, 4], [90, -1, 3], [9, 62], [-7, -1, -56, -6], [201], [76, 18]]
+##SOLUTION 1
 # a = []
 # c = []
 # for i in arr:
@@ -35,11 +36,18 @@ arr = [[8, 4], [90, -1, 3], [9, 62], [-7, -1, -56, -6], [201], [76, 18]]
 # print(c)
 
 # UPDATED CODE. FINDING MORE WAYS TO REACH SOLUTION.
+#SOLUTION 2
+# def min_sum(arr):
+#     a = 0
+#     for i in arr:
+#         a += min(i)
+#     print(a)
+#     # return sum(a)
+# minimum = min_sum(arr)
+
+# SOLUTION 3 WITH LIST COMPREHENSION
 def min_sum(arr):
-    a = 0
-    for i in arr:
-        a += min(i)
-    print(a)
-    # return sum(a)
-minimum = min_sum(arr)
+    a = sum([min(i) for i in arr])
+    return(a)
+print(min_sum(arr))
 
